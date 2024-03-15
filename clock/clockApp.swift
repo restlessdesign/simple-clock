b21bdb7296e39d964807938ddf6de9175830f558
Kevin Sweeney <hello@kevinsweeney.io>
2024-03-15 01:04:38 -0400

    Initial Commit

---
+++ clock/clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockApp.swift
+//  clock
+//
+//  Created by Kevin Sweeney on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
